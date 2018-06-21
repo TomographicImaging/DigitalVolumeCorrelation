569e83b047472376facf8b106bd7c54a41fa53e2
Edoardo Pasca <edo.paskino@gmail.com>
2018-06-21 15:24:30 +0100

    added or to preprocessor call and define target before referencing to it

--- Core/CCPiDefines.h
+++ Core/CCPiDefines.h
@@ -24,15 +24,15 @@
 #ifndef CCPIDEFINES_H
 #define CCPIDEFINES_H
 
-#if defined(_WIN32)  defined(__WIN32__)
-  #if defined(cilDVC_EXPORTS)  defined(CCPiNexusWidget_EXPORTS)  add by CMake 
+#if defined(_WIN32) || defined(__WIN32__)
+  #if defined(cilDVC_EXPORTS) ||  defined(CCPiNexusWidget_EXPORTS) //  add by CMake 
     #define  CCPI_EXPORT __declspec(dllexport)
     #define EXPIMP_TEMPLATE
   #else
     #define  CCPI_EXPORT __declspec(dllimport)
     #define EXPIMP_TEMPLATE extern
   #endif  CCPi_EXPORTS 
-#elif defined(linux)  defined(__linux)  defined(__APPLE__)
+#elif defined(linux) || defined(__linux) || defined(__APPLE__)
  #define CCPI_EXPORT
 #endif
 
