3e882a223b84bba92c07b0fcdc722c4a3003e499
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-05-18 12:45:45 +0100

    Update setup.py filepath

--- conda-recipe/gui/build.sh
+++ conda-recipe/gui/build.sh
@@ -1,6 +1,7 @@
 set -ex
 mkdir -p $SRC_DIR
-cp -rv $RECIPE_DIR/../../Apps/DVC_configurator/ $SRC_DIR
+cp -rv $RECIPE_DIR/../../Apps/DVC_configurator/ccpi $SRC_DIR
+cp -rv $RECIPE_DIR/../../Apps/DVC_configurator/setup.py $SRC_DIR/setup.py
 cd ${SRC_DIR}
 
 ${PYTHON} setup.py install
