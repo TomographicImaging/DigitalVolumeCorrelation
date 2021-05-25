be21549f5fd63447acd18b06092c01c24ac6af6a
Edoardo Pasca <edo.paskino@gmail.com>
2021-05-25 18:20:34 +0100

    remove copy of Wrappers

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,5 @@
 set -ex
 mkdir -p $SRC_DIR/ccpi
-cp -rv $RECIPE_DIR/../Wrappers/Python/ $SRC_DIR/ccpi
-
-cp -r ${RECIPE_DIR}/../Wrappers/Python/ccpi ${SRC_DIR}/ccpi/ccpi
-# cp ${RECIPE_DIR}/../setup.py ${SRC_DIR}/ccpi
 cd ${SRC_DIR}/ccpi
 
 #:: issue cmake to create setup.py
