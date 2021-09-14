64ee248b46909031926325a8571becf4b396777e
paskino <edo.paskino@gmail.com>
2021-09-14 15:26:03 +0100

    check for Mac

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,23 @@
 set -ex
 mkdir -p $SRC_DIR/ccpi
 cd ${SRC_DIR}/ccpi
-
-#:: issue cmake to create setup.py
-cmake ${RECIPE_DIR}/../  \
+if [ `python -c "from __future__ import print_function; import platform; print (platform.system())"`  == "Darwin" ] ;
+then 
+  echo "Darwin"; 
+  
+  cmake ${RECIPE_DIR}/../  \
                         -DBUILD_PYTHON_WRAPPER=OFF\
                         -DCMAKE_BUILD_TYPE="Release"\
                         -DCMAKE_INSTALL_PREFIX=$PREFIX
+                        -DOPENMP_CXX_INCLUDE_DIR=${CONDA_PREFIX}/include \
+                        -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+else 
+  echo "something else"; 
+  cmake ${RECIPE_DIR}/../  \
+                        -DBUILD_PYTHON_WRAPPER=OFF\
+                        -DCMAKE_BUILD_TYPE="Release"\
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX
+fi
 
 cmake --build . --target install
 # ${PYTHON} setup.py install
