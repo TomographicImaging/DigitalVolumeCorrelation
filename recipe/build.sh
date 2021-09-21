834b6d0692b38111c31c125fa8a41206e5a93961
Edoardo Pasca <edo.paskino@gmail.com>
2021-09-21 11:22:46 +0100

    Mac build (#154)
    * check for Mac
    
    * modify recipe and build script
    
    * add missing dso whitelist
    
    * modify CMakeLists.txt
    
    * add minimum CMake version for mac builds
    
    * removed commented out code

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,25 @@
 set -ex
 mkdir -p $SRC_DIR/ccpi
 cd ${SRC_DIR}/ccpi
-
-#:: issue cmake to create setup.py
-cmake ${RECIPE_DIR}/../  \
+if [ `python -c "from __future__ import print_function; import platform; print (platform.system())"`  == "Darwin" ] ;
+then 
+  echo "Darwin"; 
+  
+  cmake ${RECIPE_DIR}/../ -DCONDA_BUILD=ON \
+                        -DBUILD_PYTHON_WRAPPER=OFF \
+                        -DCMAKE_BUILD_TYPE="Release"\
+                        -DLIBRARY_LIB=$CONDA_PREFIX/lib \
+                        -DLIBRARY_INC=$CONDA_PREFIX \
+                        -DCMAKE_INSTALL_PREFIX=$PREFIX\
+                        -DOPENMP_INCLUDES=${CONDA_PREFIX}/include \
+                        -DOPENMP_LIBRARIES=${CONDA_PREFIX}/lib
+else 
+  echo "something else"; 
+  cmake ${RECIPE_DIR}/../  \
                         -DBUILD_PYTHON_WRAPPER=OFF\
                         -DCMAKE_BUILD_TYPE="Release"\
                         -DCMAKE_INSTALL_PREFIX=$PREFIX
+fi
 
 cmake --build . --target install
 # ${PYTHON} setup.py install
